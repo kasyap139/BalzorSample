4f04637297deae7effdde448876ba20fbd09e7f4
Kasyap Krishnan <kasyap.krishnan@leicabiosystems.com>
2020-10-15 16:35:38 +0530

    Updated Fields

--- LeicaInstallationServer.App/Pages/EmployeeOverview.cs
+++ LeicaInstallationServer.App/Pages/EmployeeOverview.cs
@@ -8,19 +8,44 @@ using System.Net.Http;
 using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using System.Net.Sockets;
 
 namespace LeicaInstallationServer.App.Pages
 {
     public partial class EmployeeOverview
     {
 		public IEnumerable<Employee> Employees { get; set; }
+        public string statusOfPC;
 
-		[Inject]
+        [Inject]
 		public IEmployeeDataService EmployeeDataService { get; set; }
 	
 		protected async override Task OnInitializedAsync()
 		{
 			Employees = (await EmployeeDataService.GetAllEmployees()).ToList();
-		}
-	}
+            statusOfPC = IsPortOpen("test");
+        }
+
+        public  string IsPortOpen(string host)
+        {
+
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect("10.10.150.130", 8080);
+                    return "Connected";
+                }
+                catch (Exception)
+                {
+                    //if (IsTurnedOn(host) == "OFF")
+                    //{
+                    //    return "Powered Off";
+                    //}
+                    //return "Host Disconnected";
+                    return "Disconnected";
+                }
+            }
+        }
+    }
 }
